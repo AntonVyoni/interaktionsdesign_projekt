5fa1e5aa4251e4bcb51a3d2379d6ff122d94c880
AntonVyoni <anton.wyoni@gmail.com>
2021-04-22 09:45:22 +0200

    added meta data extractor .py file

---
+++ metadata_extractor.py
@@ -0,0 +1,28 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+# open the image!!!!!!!!!
+image = Image.open("C:/Users/awyon/Documents/PythonProjekt/exextract/geotest.jpg")
+# extracting the exif metadata
+print(image.size)
+print(image.mode)
+print(image.format)
+exifdata = image.getexif()
+# add values to dict as key value pairs
+values = {}
+values.update({"format": image.format})
+values.update({"size": image.size})
+values.update({"mode": image.mode})
+# looping through all the tags present in exifdata
+for tagid in exifdata:   
+    # getting the tag name instead of tag id
+    tagname = TAGS.get(tagid, tagid)
+    # passing the tagid to get its respective value
+    value = exifdata.get(tagid)
+    # printing the final result
+    print(f"{tagname:25}: {value}")
+    values.update({str(tagname): value})
+
+print(values)
+with open("C:/Users/awyon/Documents/PythonProjekt/exextract/data.csv", "w") as metadata:
+    for key in values.keys():
+        metadata.write("%s,%s\n" % (key, values[key]))
